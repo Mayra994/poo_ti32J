35f6af7125ea5656e17935e9b4058cfce1983851
Mayra994 <1725110276@utectulancingo.edu.mx>
2026-05-13 14:47:51 +0000

    CREATED hola_mundo.py

---
+++ ejercicios/semana_02/hola_mundo.py
@@ -0,0 +1,11 @@
+class NombreClase:
+
+    def __init__(self):
+        print("Constructor")
+
+    def metodoUno(self):
+        print("Método uno")
+
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()
+
