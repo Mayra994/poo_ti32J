330bfc81e9d19260e280c1006c68539a3ce52fed
Mayra994 <1725110276@utectulancingo.edu.mx>
2026-05-19 23:53:29 +0000

    CREATED .gitignore

--- ejercicios/semana_02/celular.py
+++ ejercicios/semana_02/celular.py
@@ -22,4 +22,24 @@ class Celular:
         print(f"Centro de carga:{self.centro_carga}")
         print(f"Color:{self.color}")
 
+    def llamar(self):
+        print("Puedes realizar llamadas")   
+
+    def enviar_mensajes(self):
+        print("Puedes enviar mensajes de texto")
+
+    def tomar_fotos(self):
+        print("Puedes tomar fotos")   
+
+    def reproducir_musica(self):
+        print("Puedes escuchar tus canciones favoritas")   
+
+    def apagar(self):
+        print("Puedes apagar tu celular")           
+
 Samsung_S24_Ultra = Celular("Dynamic AMOLED 2x de 6,8 pulgadas","EB-BS928aby","200mp(ISOCELL hp2sx)", None, "Boton de volumen y boton lateral","Inferior y superior","Sensor ultrasonico","Teleobjetivos con sensor de 50mp","Tipo C","Violeta Titanio")        
+Samsung_S24_Ultra.llamar()
+Samsung_S24_Ultra.enviar_mensajes()
+Samsung_S24_Ultra.tomar_fotos()         
+Samsung_S24_Ultra.reproducir_musica()
+Samsung_S24_Ultra.apagar()
